d8867c06442c5e42ac9f57dc24b445a33e233d2d
Rahul Bhatt <rbhatt.cs@gmail.com>
2025-07-04 21:40:13 +0530

    Simpler queue methods

--- DSA_2_Queues.java
+++ DSA_2_Queues.java
@@ -1,5 +1,25 @@
+import java.util.*;
 public class DSA_2_Queues {
     public static void main(String[] args){
 
+//        Queue is an interface not a class so we need a class that inplements a Queue.
+        Queue<String> queue = new LinkedList<String>();
+
+        queue.offer("Rahul");
+        queue.offer("Yash");
+        queue.offer("Steve");
+        queue.offer("Harold");
+
+        System.out.println(queue);
+
+        queue.poll();
+
+        System.out.println(queue);
+
+        System.out.println(queue.peek());
+//        We can have 3 methods for queue:
+//        offer(e) to add an element to the tail.
+//        poll() to remove the element at head.
+//        peek() used to get a peek at what is the element at the head.
     }
 }
