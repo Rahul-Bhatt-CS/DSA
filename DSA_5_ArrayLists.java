2053d5e5847021355a25ae370d1803af9dc1f0b7
Rahul Bhatt <rbhatt.cs@gmail.com>
2025-07-16 11:07:34 +0530

    test

---
+++ DSA_5_ArrayLists.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class DSA_5_ArrayLists {
+    public static void main(String[] args) {
+        int[] array = new int[20];
+//        this is a static array which is size immutable and takes same space even if no element is stored.
+//        on the other hand a dynamic array has a mutable size.
+
+//        a dynamic array uses different static arrays to store a set of values.
+//        when the previous array is filled completely then the compiler defines a new array with increased size.
+//        for java the growth factor is 1.5 times. i.e. the new array initialized will be 1.5 times the previous one.
+
+//        Advantages of ArrayLists:
+//              we can do random access in O(1) time;
+//              Good locality of reference and data cache utilization.
+//              Easy to insert and delete at the end.
+    }
+}
